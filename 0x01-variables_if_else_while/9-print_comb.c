cdfe1b81082edb51894fbc3d776ebe29fe23f28c
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-09 16:08:35 +0000

    Combination of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - print all possible combinations of single-digit numbers
+ *Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
