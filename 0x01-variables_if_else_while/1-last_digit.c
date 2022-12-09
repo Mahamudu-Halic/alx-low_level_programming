e285ee35df5b58dd44e9e3d68ef1f5e2c18a0d0c
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-09 14:26:54 +0000

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,8 +6,7 @@
 /**
  * main - assign a random number to the variable n each time it is excuted
  * print the last digit of the number stored in the variable n.
- * 
- * return (0); (Success)
+ * Return Always 0 (Success)
 */
 int main(void)
 {
