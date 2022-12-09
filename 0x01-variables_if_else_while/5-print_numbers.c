b7144095e4a8a4b2d3170d15693ed879aa2b07c8
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-09 15:16:54 +0000

    Print digit less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - print single digit numbers of base 10 starting from 0
+ *Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
