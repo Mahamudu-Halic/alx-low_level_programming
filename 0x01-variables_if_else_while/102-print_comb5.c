b6c9220cc2c68df320283aa370c51cb3cc4c42ae
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-10 11:21:53 +0000

    Combines two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
 /**
- *main - print all possible combination of two two-digits numbers
- *Return: Always 0.
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
 int main(void)
-{
-	int i;
-	int j;
-	int k;
-	int l;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			for (k = i; k < 10; k++)
-			{
-				for (l = j; l < 10; l++)
-				{
-					if (k == 0 && l == 0)
-						continue;
-					putchar((i % 10) + '0');
-					putchar((j % 10) + '0');
-					putchar(' ');
-					putchar((k % 10) + '0');
-					putchar((l % 10) + '0');
-					if (i == 9 && j == 8)
-						break;
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+
+
+{
+int first_nums;
+int second_nums;
+
+
+for (first_nums = 0; first_nums < 100 ; first_nums++)
+{
+for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
+{
+
+putchar (first_nums / 10 + '0');
+putchar (first_nums % 10 + '0');
+
+
+putchar (' ');
+
+putchar (second_nums / 10 + '0');
+putchar (second_nums % 10 + '0');
+
+
+if (first_nums == 98 && second_nums == 99)
+{
+break;
+}
+
+
+putchar(',');
+putchar (' ');
+
+
+}
+
+}
+
+putchar('\n');
+
+
+return (0);
+
 }
