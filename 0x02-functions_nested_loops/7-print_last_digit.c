e96bc01a47f892ef1fe7206b7b7d6dae057e9b76
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-13 23:58:02 +0000

    Print last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,13 @@ int print_last_digit(int n)
 	if (n >= 0)
 	{
 		last_digit = n % 10;
+		_putchar(last_digit + '0');
 		return (last_digit);
 	}
 	else
 	{
 		last_digit = -(n % 10);
+		_putchar(lat_digit + '0');
 		return (last_digit);
 	}
 }
