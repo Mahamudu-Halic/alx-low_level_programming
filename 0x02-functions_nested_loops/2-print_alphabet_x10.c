d1dc5af5a0a0364f8c002f21a0358b2153f6a640
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-13 20:31:29 +0000

    Prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints lowercase alphabets 10 times
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	char alphabet;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
