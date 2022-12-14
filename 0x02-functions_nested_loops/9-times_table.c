f165db8828867093af1883a30c00aa50806cd495
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-14 01:42:11 +0000

    print the 9 times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,24 +10,33 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		for (j = 1; j < 10; j++)
 		{
 			product = i * j;
 
-			if (product > 9)
+			if (product == 81)
 			{
+				_putchar(' ');
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
+				break;
+			}
+			if (product > 9)
+			{
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+				if (j != 9)
+					_putchar(',');
 			}
 			else
 			{
-				_putchar(product + '0');
-			}
-
-			if (j != 9)
-			{
-				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+				if (j != 9)
+					_putchar(',');
 			}
 		}
 		_putchar('\n');
