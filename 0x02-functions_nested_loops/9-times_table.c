faed21654977cdbf96170f1aa81a7dd56a2fdaa7
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-14 01:03:07 +0000

    print the 9 times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *times_table - prints the 9 times table
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			product = i * j;
+
+			if (product > 9)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else
+			{
+				_putchar(product + '0');
+			}
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
