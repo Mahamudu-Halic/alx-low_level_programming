f4e81ad24d10962718808810d2cfe57f3a6557a7
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-15 09:42:44 +0000

    avoiding infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
         i = 0;
-	/**
+	/*
         *while (i < 10)
         *{
+	*	 Infinite loop - No increament in value
         *        putchar(i);
         *}
 	*/
         printf("Infinite loop avoided! \\o/\n");
-
         return (0);
 }
