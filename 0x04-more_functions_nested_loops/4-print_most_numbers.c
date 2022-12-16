afca879fc50ca27a55649005a313cba6a373dc48
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-15 23:14:53 -0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- *print_more_numbers - prints the numbers from 0 to 9
- */
-void print_more_numbers(void)
+*print_most_numbers - prints
+*/
+
+void print_most_numbers(void)
 {
-	int i;
-	for (i = 0; i <= 9; i++)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(i == 2 || i == 4))
-			_putchar(i + '0');
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
