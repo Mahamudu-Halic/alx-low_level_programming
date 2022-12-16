8c3cea2e541d0d0f449904e00abb8ba8bd79a085
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-15 23:47:56 -0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,6 +7,8 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
+	if (size > 0)
+	{
 		for (i = 1; i <= size; i++)
 		{
 			for (j = i + 1; j <= size; j++)
@@ -20,5 +22,12 @@ void print_triangle(int size)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
+#include "main.h"
+
+
+
