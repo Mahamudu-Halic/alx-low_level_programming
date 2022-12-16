de5125f38067cb31aa882b35c293bae902dd6825
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-15 23:13:33 -0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,7 @@ void print_more_numbers(void)
 	int i;
 	for (i = 0; i <= 9; i++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		else
+		if (!(i == 2 || i == 4))
 			_putchar(i + '0');
 	}
 	_putchar('\n');
