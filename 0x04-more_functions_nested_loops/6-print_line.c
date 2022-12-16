bbf894759fc392ea88d7b9e7c8cc4f37b8284b28
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-15 23:20:06 -0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *print_line - draws a straight line in the terminal
+ *print_line - draws a straight 
+ *line in the terminal
  *@n: value to be computed
  */
 void print_line(int n)
