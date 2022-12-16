1f8502260751fe01f7f48cbd1a6fbe5e0b65416b
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-15 23:39:14 -0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,20 +6,20 @@
  *prints fizzbuzz if multible of both 3 and 5
  *return: Always 0.
  */
-int main(void)
+ int main(void)
 {
 	int i;
 
-	for (i = 1; i <=100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%d", i);
+			printf("%i", i);
 		if (i < 100)
 			printf(" ");
 	}
