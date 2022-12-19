9e4bf2c4c1882de36cbd5112080d22ba2ac406ba
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-19 17:21:13 +0000

    print multiple of 2 characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *puts2 - prints multiple of two in a string
+ *@str: value to be inputed
+ */
+void puts2(char *str)
+{
+	int i;
+	int n = 0;
+	
+	while (str[n] != '\0')
+		n++;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
