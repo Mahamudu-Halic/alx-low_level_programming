b65ea032309400a053af83bea2d1d9d8a0b9f9e3
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-19 15:13:01 +0000

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: value to be entered
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
