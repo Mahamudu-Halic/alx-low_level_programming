9637c894fbc2f61371098bcbbd824d364469105d
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-19 17:58:19 +0000

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - print half of a string
+ * @str: value to be inputed
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+	int i, j, strlen;
+
+	while (str[n] != '\0')
+		n++;
+	if (strlen % 2 != 0)
+		j = (strlen - 1) / 2;
+	else 
+		j = strlen / 2;
+	strlen = n;
+	for (i = j; i < strlen; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
