745f744c9c09d3a466ebbf9379cf42dddb67d985
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-19 19:46:50 +0000

    print numbers in array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - print array of numbers
+ *@a: array to be inputed
+ *@n: number of times array runs
+ */
+void print_array(int *a; int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i + 1 != n)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
