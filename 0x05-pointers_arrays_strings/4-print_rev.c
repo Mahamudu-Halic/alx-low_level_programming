8971747a2a6f6ceb0eda49d664ccb5d8f62d555c
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-19 15:29:53 +0000

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_rev - prints string in reverse
+ *@s: value to be entered
+ */
+void print_rev(char *s)
+{
+	int n = 0;
+	int i;
+
+	for (; *s++;)
+		n++;
+
+	for (i = n; i <= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
