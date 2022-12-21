3b6aa3b43276bd46256b0ec20e17175d46a7266e
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-21 22:12:01 +0000

    concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: destination of concatenated text
+ *@src: source of concatenated text
+ *Return: results of concatenated text
+ */
+char *_strcat(char *dest, char *src)
+{
+        return (strcat(dest, src));
+}
