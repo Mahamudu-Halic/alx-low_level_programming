318ec60bffefb5db22f838afe500fe9533739ae9
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-22 02:38:00 +0000

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,31 @@
 #include "main.h"
+
 /**
- *cap_string - capitalize all words of a string
- *@a: string
- *Return: capitalized string
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
  */
-char *cap_string(char *a)
+char *cap_string(char *s)
 {
-	int i, counter = 1;
-	for (i = 0; i < strlen(a); i++)
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
 	{
-		if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			counter = 1;
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
-		else if (counter == 1)
-		{
-			a[i] = toupper(a[i]);
-			counter = 0;
-		}
+		i++;
 	}
-	return (a);
+	return (s);
 }
