d67edacc85192a696070bf2def2305516ee1fd53
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2022-12-22 00:33:59 +0000

    Header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <string.h>
 #include <ctype.h>
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,3 +11,4 @@ char _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+#endif
