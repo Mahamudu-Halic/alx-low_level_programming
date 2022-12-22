fd28cc9dbd9e430047abd19e53ee6204c4453781
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-22 02:41:09 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,5 +7,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        return (strcat(dest, src));
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
