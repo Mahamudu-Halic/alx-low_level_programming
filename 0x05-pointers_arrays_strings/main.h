80033d3f0f3fec5a61f1a994b5bd5c4791fee00d
Mahamudu Halic <mahamuduhalic129@gmail.com>
2022-12-23 15:43:16 -0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+#endif
