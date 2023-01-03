761e3cb9db9fecbb4e3c2b8ac99ac6e415e8ad22
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-03 13:55:06 -0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,14 +5,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-int i, j;
+	unsigned int i, m = 0;
 
-for (i = 0; i < 8; i++)
-{
-for (j = 0; j < 8; j++)
-{
-putchar(a[i][j]);
-}
-printf("\n");
-}
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
+	}
+	_putchar('\n');
 }
