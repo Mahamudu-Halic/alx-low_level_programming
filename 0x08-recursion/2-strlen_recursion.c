bbcf877a06262c0011bd6359862fe2dd49f0a826
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-05 20:03:32 -0100

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strlen_recursion - print length of string
+ *@s: string to be read
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\n')
+{
+return (1 + _strlen_recursion(s + 1));   
+}
+return 0;
+}
