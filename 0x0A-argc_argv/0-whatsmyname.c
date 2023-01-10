dcac667bcce3477487f895a9741c873c11e65326
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-10 03:54:27 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
  *@argv: array of command passed to tbe code
  *Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void) argc;
+	(void)argc;
 
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
