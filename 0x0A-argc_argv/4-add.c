082edb6b93ecfe0d4f7b6ae10c59eff94557bf47
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2023-01-10 04:21:41 +0000

    0x0A argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ *main - adds positive numbers.
+ *@argc: number of arguments
+ *@argv: arguments
+ *Return: 0, otherwise 1
+ */
 int main(int argc, char **argv)
 {
 	int i, add = 0;
+	char *flag;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (argv[i] >= 0)
-			{
-				add += atoi(argv[i]);
-			}
-			else
+			strtol(argv[i], &flag, 10);
+			if(*flag)
 			{
 				printf("Error\n");
 				return (1);
+			}
+			else
+			{
+				add += atoi(argv[i]);
 			}
 		}
 		printf("%d\n", add);
