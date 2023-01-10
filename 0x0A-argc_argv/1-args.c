518dba0281b037c48a2a3ef02adb6e08aed363a4
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-10 03:55:44 +0000

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,10 @@
  *@argv: array of commands and arguments
  *Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	(void)argv;
+
 	printf("%d\n", argc - 1);
-	return 0;
+	return (0);
 }
