64bd0d49abf7ec7d1445d2bc20505f95915fc076
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-10 04:26:52 +0000

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,29 +8,29 @@
  */
 int main(int argc, char **argv)
 {
-	int i, add = 0;
+	int i, n, sum = 0;
 	char *flag;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			strtol(argv[i], &flag, 10);
-			if(*flag)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				add += atoi(argv[i]);
-			}
-		}
-		printf("%d\n", add);
-	}
-	else
+	if (argc < 2)
 	{
 		printf("0\n");
+		return (0);
 	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
 	return (0);
 }
