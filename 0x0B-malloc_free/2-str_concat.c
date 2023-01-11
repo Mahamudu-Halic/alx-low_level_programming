56eb0a14ad2cee42d08ecd596e0a4fc4fcb60d8c
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-11 04:45:31 +0000

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,38 +4,30 @@
  *@s1: first string
  *@s2: second string
  *Return: concatenated string
- */
-char *str_concat(char *s1, char *s2)
+ */char *str_concat(char *s1, char *s2)
 {
-char *ptr;
-int s1Size = strlen(s1);
-int s2Size = strlen(s2);
-int i;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-ptr = malloc(sizeof(char) * (s1Size + s2Size));
-if (ptr == NULL)
-return (NULL);
-for (i = 0; i < s1Size; i++)
-{
-ptr[i] = s1[i];
-  if (ptr[i] == NULL)
-  {
-  free(ptr);
-  return (NULL);
-  }
-}
-for (i = 0; i < s2Size; i++, s1Size++)
-{
-ptr[s1Size] = s2[i];
-  if (ptr[i] == NULL)
-  {
-  free(ptr);
-  return (NULL);
-  }
-}
-return (ptr);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
