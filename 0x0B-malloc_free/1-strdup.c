44016dfd61836dba25dfd3a1d7b6f6de8aa96ea4
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-11 04:27:54 +0000

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,6 +5,25 @@
  *Return:  returns a pointer to a newly allocated space in memory
  */
 char *_strdup(char *str)
-{    
-return (strdup(str));
+{
+	char *duplicate;
+	int index, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
