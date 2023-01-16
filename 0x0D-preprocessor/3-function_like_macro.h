76b5ae450aa6668b80c14023748cc5d2bd9b05bf
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-16 14:30:12 +0000

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef ABS_H
+#define ABS_H
 
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif /* ABS_H */
