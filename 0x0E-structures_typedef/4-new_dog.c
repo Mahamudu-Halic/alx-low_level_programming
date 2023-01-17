e4063e53acc05e0ed107d30ca857a0b1b9738433
Mahamudu Halic <mahamuduhalic129@gmail.com>
2023-01-17 01:25:29 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,17 +6,40 @@
  *@owner: owner of dog
  *Return: a copy of dog info
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	unsigned int nl, ol, i;
 	dog_t *dog;
-	dog = malloc(sizeof(dog_t));
 
-	if (dog == NULL)
-		return (NULL);
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog->name = name;
-	dog->owner = owner;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
 	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
 	return (dog);
 }
